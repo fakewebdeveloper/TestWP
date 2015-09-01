7e99e0d4925c82f36788a1b517061ff92f603706
Fake web developer <fakewebdeveloper@gmail.com>
2015-09-02 01:56:42 +0530

    Added source.

---
+++ da_time_slot.php
@@ -0,0 +1,18 @@
+<?php
+?>
+<div class="wrap main_wrapper">
+	<div class="content_wraper" id="content_wraper">
+		<h3 class="page_title">Manage Time Slots</h3>
+		
+		<div class="page_container">
+			<form action="" method="post" class="da_time_slot_form">
+				<table>
+					<tr>
+						<td></td>
+						<td></td>
+					</tr>
+				</table>
+			</form>
+		</div>
+	</div>
+</div>
